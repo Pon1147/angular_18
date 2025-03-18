bc52c01484fcc68f09801211a313770fb95b76df
viklatao117 <khoale1147@gmail.com>
2025-03-18 16:14:19 +0700

    Angular Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HelloComponent } from "./Components/hello/hello.component";
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -7,8 +7,18 @@ import { HelloComponent } from "./Components/hello/hello.component";
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HelloComponent]
+  imports: [FormsModule] // import FormModule để sử dụng
 })
 export class AppComponent {
   title = 'research-v18';
+  user = { // khai báo obj user
+    name: 'Khoa Lê', // khai báo các phần tử
+    age: 28,
+    color: 'Yellow',
+  }
+  imgURL = 'https://images2.thanhnien.vn/zoom/700_438/528068263637045248/2024/1/26/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912-37-0-587-880-crop-1706239860681642023140.jpg'
+  showInfor(){
+    alert ('Nice, bạn vừa click vào button')
+  }
+  userName =''; // khai báo obj user
 }
