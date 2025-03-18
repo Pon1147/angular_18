377fa3d29bf0cdb28f932e53f0bb3e378002cacc
viklatao117 <khoale1147@gmail.com>
2025-03-18 15:36:55 +0700

    EXPLORE ANGULAR APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from "./Components/hello/hello.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HelloComponent]
 })
 export class AppComponent {
   title = 'research-v18';
