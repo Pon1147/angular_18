a8c17aaa4bb7bc739692317f80ca5f0d0d9fb0d9
viklatao117 <khoale1147@gmail.com>
2025-03-20 11:06:09 +0700

    ANGULAR STRUCTURE DIRECTIVE NGFOROF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,42 @@ import { CommonModule } from '@angular/common';
   imports: [FormsModule, CommonModule],
 })
 export class AppComponent {
+  imgURL = '/img/img_1.png'
   title = 'research-v18';
-
+  items = [
+    {
+      id: 1, name: 'Kyle'
+    },
+    {
+      id: 2, name: 'Sarah'
+    },
+    {
+      id: 3, name: 'Michael'
+    }];
+  codeSnippet = `
+  <ul>
+    <li *ngFor="let item of items; index as i; first as isFirst; last as isLast">
+      {{ i + 1 }}. {{ item }} (First: {{ isFirst }}, Last: {{ isLast }})
+    </li>
+  </ul>`;
+  show = '<ng-template>:';
+  show1 = `<ng-container>`
+  show2 = '{{item}}';
+  show3 = `
+  <ul>
+    <li *ngFor="let item of items; trackBy: trackByFn">{{ item.name }}</li>
+  </ul>`;
+  show4 = `
+  trackByFn(index: number, item: { id: number; name: string }): number {
+  return item.id;
+  }`;
+  show5 = `
+  <ng-template ngFor let-item [ngForOf]="items" let-i="index">
+      <div>{{ i + 1 }}. {{ item }}</div>
+  </ng-template>`;
+  show6 = `<!-- Không hợp lệ -->
+      <li *ngIf="items.length > 0" *ngFor="let item of items">{{ item.name }}</li>`;
+  trackByFn(index: number, item: { id: number; name: string }): number {
+    return item.id; // Trả về giá trị duy nhất để theo dõi
+  }
 }
