fc5674be859e25bcc536dd1c037bbeb6c6fed223
viklatao117 <khoale1147@gmail.com>
2025-03-24 14:53:52 +0700

    Input, OutPut, EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from "@angular/core";
-import { ResearchComponent } from "./components/research/research.component";
+import { ProgressComponent } from "./components/progress/progress.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   styleUrls: ["./app.component.scss"],
   templateUrl: "./app.component.html",
-  imports: [ResearchComponent],
+  imports: [ProgressComponent],
 
 })
-export class AppComponent { }
+export class AppComponent {
+  progress = 70;
+  backgrounColor = 'black';
+  progressColor = 'blue';
+  title = "Research v18";
+  // currentTask = 'Research Angular 18';
+  label1 = ' @Input() tenBien: KieuDuLieu;';
+  label2 = ' <child-component [tenBien]="giaTri"></child-component>';
+  label3 = `<child-component (tenSuKien)="hamXuLy($event)">.`;
+  currentTask = 'Học Angular 18';
+  status = 'Chưa hoàn thành';
+
+  onTaskCompleted(isCompleted: boolean) {
+    this.progress = this.progress + 1;
+    this.status = isCompleted ? 'Đã hoàn thành' : 'Chưa hoàn thành';
+  }
+
+}
 
