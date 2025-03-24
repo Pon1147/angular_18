a783047ff73482606df7b2e6618636ea04bf37f2
viklatao117 <khoale1147@gmail.com>
2025-03-24 10:11:06 +0700

    ATTRIBUTE DIRECTIVE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,44 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
+  imports: [CommonModule],  // add NgModule to import and export components and modules in the app
+  standalone: true,  // remove declarations from the component metadata
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [FormsModule, CommonModule],
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  imgURL = '/img/img_1.png'
+
   title = 'research-v18';
-  items = [
-    {
-      id: 1, name: 'Kyle'
-    },
-    {
-      id: 2, name: 'Sarah'
-    },
-    {
-      id: 3, name: 'Michael'
-    }];
-  codeSnippet = `
-  <ul>
-    <li *ngFor="let item of items; index as i; first as isFirst; last as isLast">
-      {{ i + 1 }}. {{ item }} (First: {{ isFirst }}, Last: {{ isLast }})
-    </li>
-  </ul>`;
-  show = '<ng-template>:';
-  show1 = `<ng-container>`
-  show2 = '{{item}}';
-  show3 = `
-  <ul>
-    <li *ngFor="let item of items; trackBy: trackByFn">{{ item.name }}</li>
-  </ul>`;
-  show4 = `
-  trackByFn(index: number, item: { id: number; name: string }): number {
-  return item.id;
-  }`;
-  show5 = `
-  <ng-template ngFor let-item [ngForOf]="items" let-i="index">
-      <div>{{ i + 1 }}. {{ item }}</div>
-  </ng-template>`;
-  show6 = `<!-- Không hợp lệ -->
-      <li *ngIf="items.length > 0" *ngFor="let item of items">{{ item.name }}</li>`;
-  trackByFn(index: number, item: { id: number; name: string }): number {
-    return item.id; // Trả về giá trị duy nhất để theo dõi
+  label1 = `<style>`;
+  label2 = `<div [class.active]="isActive">Nội dung</div>`
+  label3 = `// Trong file component.ts
+export class AppComponent {
+  isActive = true;
+  }  `;
+  label4 = ` <div class ="active">`;
+  // Example 1 [class.active] => nếu acctice = true --> thẻ có class
+  tasks = [
+    { id: 1, name: 'Học Angular 18', completed: false },
+    { id: 2, name: 'Thực hành', completed: true },
+    { id: 3, name: 'Tìm hiểu', completed: false },
+  ];
+
+  toggleTaskCompletion(taskID: number) {
+    const task = this.tasks.find(t => t.id === taskID);
+    if (task) {
+      task.completed = !task.completed;
+    }
   }
+  handleKeyUp(event: KeyboardEvent, taskId: number) {
+    if (event.key === 'Enter' || event.key === ' ') { // Hỗ trợ phím Enter và Space
+      this.toggleTaskCompletion(taskId);
+    }
+  }
+
+  // Example 2 [style.background-color] (nếu true => thẻ sẽ có style background-color)
+  isActive = true;
+  progress = 75;
 }
