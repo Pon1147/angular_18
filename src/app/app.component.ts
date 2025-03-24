a75d6ab4b0432c144685c468f278c056b5b87ce6
viklatao117 <khoale1147@gmail.com>
2025-03-24 13:28:25 +0700

    Rules in JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { ResearchComponent } from "./components/research/research.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [CommonModule],  // add NgModule to import and export components and modules in the app
-  standalone: true,  // remove declarations from the component metadata
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  standalone: true,
+  styleUrls: ["./app.component.scss"],
+  templateUrl: "./app.component.html",
+  imports: [ResearchComponent],
+
 })
+export class AppComponent { }
 
-export class AppComponent {
-
-  title = 'research-v18';
-  label1 = `<style>`;
-  label2 = `<div [class.active]="isActive">Nội dung</div>`
-  label3 = `// Trong file component.ts
-export class AppComponent {
-  isActive = true;
-  }  `;
-  label4 = ` <div class ="active">`;
-  // Example 1 [class.active] => nếu acctice = true --> thẻ có class
-  tasks = [
-    { id: 1, name: 'Học Angular 18', completed: false },
-    { id: 2, name: 'Thực hành', completed: true },
-    { id: 3, name: 'Tìm hiểu', completed: false },
-  ];
-
-  toggleTaskCompletion(taskID: number) {
-    const task = this.tasks.find(t => t.id === taskID);
-    if (task) {
-      task.completed = !task.completed;
-    }
-  }
-  handleKeyUp(event: KeyboardEvent, taskId: number) {
-    if (event.key === 'Enter' || event.key === ' ') { // Hỗ trợ phím Enter và Space
-      this.toggleTaskCompletion(taskId);
-    }
-  }
-
-  // Example 2 [style.background-color] (nếu true => thẻ sẽ có style background-color)
-  isActive = true;
-  progress = 75;
-}
